9d166ca045a456b2056d874eef83c342eeb30ac4
Atsuhiro <atcheri@gmail.com>
2024-12-14 22:46:33 +0100

    fix: rename the exported ts sources

---
+++ anchor/src/crud-exports.ts
@@ -0,0 +1,16 @@
+// Here we export some useful types and functions for interacting with the Anchor program.
+import { AnchorProvider, Program } from '@coral-xyz/anchor'
+import { PublicKey } from '@solana/web3.js'
+import CrudIDL from '../target/idl/crud.json'
+import type { Crud } from '../target/types/crud'
+
+// Re-export the generated IDL and type
+export { Crud, CrudIDL }
+
+// The programId is imported from the program IDL.
+export const BASIC_PROGRAM_ID = new PublicKey(CrudIDL.address)
+
+// This is a helper function to get the Basic Anchor program.
+export function getBasicProgram(provider: AnchorProvider) {
+  return new Program(CrudIDL as Crud, provider)
+}
